3bac0d3c1272567bfa9fc238ebd77a22f8125908
AndyNguyenF4 <bboysir03@gmail.com>
2024-08-17 01:52:05 -0500

    Added nonfunctional etro import and window renaming

--- BiSTracker/Windows/MainWindow.cs
+++ BiSTracker/Windows/MainWindow.cs
@@ -11,13 +11,14 @@ namespace BiSTracker.Windows;
 public class MainWindow : Window, IDisposable
 {
     private string GoatImagePath;
+    private string etroImportString = "";
     private Plugin Plugin;
 
     // We give this window a hidden ID using ##
     // So that the user will see "My Amazing Window" as window title,
     // but for ImGui the ID is "My Amazing Window##With a hidden ID"
     public MainWindow(Plugin plugin, string goatImagePath)
-        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+        : base("BiS Tracker##BiSTrackerDisplay", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
         {
@@ -33,6 +34,12 @@ public class MainWindow : Window, IDisposable
 
     public override void Draw()
     {
+        //etro links are 60 chars long, 61 for C# esc char?
+        
+        ImGui.InputTextWithHint("##EtroImportTextBox", "Insert Etro URL and click \"Import\"", ref etroImportString, 61);
+        ImGui.SameLine();
+        ImGui.Button("Import");
+        
         ImGui.Text($"The random config bool is {Plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
 
         if (ImGui.Button("Show Settings"))
@@ -56,4 +63,5 @@ public class MainWindow : Window, IDisposable
         }
 
     }
+
 }
